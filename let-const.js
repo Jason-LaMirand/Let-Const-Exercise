a9b182eb96ae5d7ebb08624ac84d47a6cc310b31
Jason-LaMirand <jaylamirand@gmail.com>
2022-05-02 20:25:40 -0500

    added code to the files

---
+++ let-const.js
@@ -0,0 +1,16 @@
+// ES5 Global Constants
+const PI = 3.14;
+PI = 42; // stop me from doing this!
+console.log(PI);
+
+
+
+//Quiz
+//1.What is the difference between var and let?
+//  var can be reassigned and redecalred, while let can not be redecalred, but it can be reassigned.
+//2.What is the difference between var and const?
+//  var can be redecalred and reassigned, while const can do neither.
+//3.What is the difference between let and const?
+//  let can be reassigned and const can not.
+//4.What is hoisting?
+//  A variable sitting above the function. That can be used before they are actually declared in the function.
